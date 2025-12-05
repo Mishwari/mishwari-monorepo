6499a1a259b67234b5b89055aeae59a57be9f884
husniabad <husni.abad.97@gmail.com>
2025-12-05 17:56:17 +0200

    remove font preload

--- packages/utils/src/firebase/constants.ts
+++ packages/utils/src/firebase/constants.ts
@@ -1,4 +1,4 @@
-export const FIREBASE_ENABLED_COUNTRY_CODES = ['967', '20'];
+export const FIREBASE_ENABLED_COUNTRY_CODES = ['967', '20', '91', '966'];
 
 export const shouldUseFirebase = (phoneNumber: string): boolean => {
   return FIREBASE_ENABLED_COUNTRY_CODES.some(code => phoneNumber.startsWith(code));
