dd98b32b7c5531d02d772e7b86e4e6a51e943036
husniabad <husni.abad.97@gmail.com>
2025-12-16 15:30:22 +0200

    add 92 for firebase option

--- packages/utils/src/firebase/constants.ts
+++ packages/utils/src/firebase/constants.ts
@@ -1,4 +1,4 @@
-export const FIREBASE_ENABLED_COUNTRY_CODES = ['967', '20', '91', '966', '971', '964', '974', '968', '965', '973', '971','1'];
+export const FIREBASE_ENABLED_COUNTRY_CODES = ['967', '20', '91', '966', '971', '964', '974', '968', '965', '973', '971','1', '92'];
 
 export const shouldUseFirebase = (phoneNumber: string): boolean => {
   return FIREBASE_ENABLED_COUNTRY_CODES.some(code => phoneNumber.startsWith(code));
